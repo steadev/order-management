ec4c82a63e84aa386340563158acbddefb63938f
MyungJoo Lee <audwn0326@gmail.com>
2023-01-18 23:18:09 +0900

    feat: add table view

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,18 @@ class App extends StatelessWidget {
       title: '주문 내역',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('주문 내역'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Table(
+          border: TableBorder.all(),
+          children: [
+            buildRow(['이름', '연락처', '송금확인', '배송확인'])
+          ],
         ),
       ),
     );
   }
+
+  TableRow buildRow(List<String> cells) =>
+      TableRow(children: cells.map((cell) => Text(cell)).toList());
 }
