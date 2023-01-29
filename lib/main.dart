d301e103dab595effbff5e43b43bfd51d44220cc
MyungJoo Lee <audwn0326@gmail.com>
2023-01-29 23:36:10 +0900

    feat: add bottonNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -84,6 +84,22 @@ class App extends StatelessWidget {
             textSection
           ],
         ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Business',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'School',
+            ),
+          ],
+        ),
       ),
     );
   }
